7e05d4339db79d45941445007008e664c3e591f5
Mohamed <git@heroku.com:madani.git>
2015-02-01 13:31:42 +0100

    a

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Coming soon!');
+  response.send(request);
+
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+app.listen(app.get('port'));
